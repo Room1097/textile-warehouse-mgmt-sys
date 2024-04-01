ccb6d7288f54cbb761e84594ad5f4d54c337323e
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-04-01 20:26:07 +0530

    added .env

--- projectbackend/index.js
+++ projectbackend/index.js
@@ -1,10 +1,13 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 var cors = require('cors')
 app.use(cors());
-const PORT = 3001;
-app.listen(PORT,()=>{
-    console.log(`http://localhost:${PORT}`);
+
+const port = process.env.PORT
+
+app.listen(port,()=>{
+    console.log(`http://localhost:${port}`);
 })
 app.get("/",(req,res)=>{
     res.json({msg:"Hello World"});
