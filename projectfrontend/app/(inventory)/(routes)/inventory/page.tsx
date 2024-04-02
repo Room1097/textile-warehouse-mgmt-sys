8a260e6ea914e8306fd264023a28c72edd8e4d85
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-04-02 14:28:57 +0530

    fixed zod installed in wrong place

--- projectfrontend/app/(inventory)/(routes)/inventory/page.tsx
+++ projectfrontend/app/(inventory)/(routes)/inventory/page.tsx
@@ -1,22 +1,17 @@
-import { z } from 'zod'
+import { z } from "zod";
 
-const materialSchema = z.object({
+const materialSchema = z.object({});
 
-})
- 
 export default function Material() {
   return (
     <div className="flex flex-col gap-8">
       <h1 className="text-3xl pt-4">Inventory</h1>
       <div>
         <h3>Add Material</h3>
-        <form action="">
-
-        </form>
+        <form action=""></form>
       </div>
       <div>
         <h3>In Stock</h3>
-        
       </div>
     </div>
   );
