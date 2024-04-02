3f6c45f181b9fd937dab4a698458adfe58de5e97
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-04-03 00:11:20 +0530

    fixed submit functionality for inserting raw materials

--- projectfrontend/components/RawMaterial/RawMaterialItem/rawMaterialForm.tsx
+++ projectfrontend/components/RawMaterial/RawMaterialItem/rawMaterialForm.tsx
@@ -1,31 +1,20 @@
-import React from "react";
-import { ZodType, z } from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
+"use client";
+import React, { useState } from "react";
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 
-type supply = {
-  rid: number;
-  sid: number;
-  wt: number;
-};
+import { ZodType, z } from "zod";
 
-type rawMaterial = {
+type RawMaterial = {
   r_name: string;
   r_color: string;
   denier: number;
   filament: number;
 };
 
-const supplySchema = z.object({
-  rid: z.number(),
-  sid: z.number(),
-  wt: z.number().nonnegative(),
-});
-
-const rawMaterialSchema = z.object({
+const rawMaterialSchema: ZodType<RawMaterial> = z.object({
   r_name: z.string().min(2).max(30),
   r_color: z.string().min(3).max(10),
   denier: z.number().nonnegative(),
@@ -33,60 +22,101 @@ const rawMaterialSchema = z.object({
 });
 
 const RawMaterialForm = () => {
-  const { register, reset, handleSubmit } = useForm<rawMaterial>({
-    resolver: zodResolver(rawMaterialSchema),
+  const [formData, setFormData] = useState<RawMaterial>({
+    r_name: "",
+    r_color: "",
+    denier: 0,
+    filament: 0,
   });
 
-  const submitData = (data: rawMaterial) => {
-    fetch("http://localhost:3001/api/raw", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        r_name: data.r_name,
-        r_color: data.r_color,
-        denier: data.denier,
-        filament: data.filament,
-      }),
-    }).then((res) => {
-      res.json();
-    });
-    reset();
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    const numericValue = parseInt(value); 
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: isNaN(numericValue) ? value : numericValue, 
+    }));
+  };
+
+  const submitRawData = () => {
+    const validationResult = rawMaterialSchema.safeParse(formData);
+    if (validationResult.success) {
+      fetch("http://localhost:3001/api/raw", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          R_name: formData.r_name,
+          R_color: formData.r_color,
+          denier: formData.denier,
+          no_of_filaments: formData.filament,
+        }),
+      })
+        .then((res) => {
+          if (res.ok) {
+            console.log("Data submitted successfully");
+          } else {
+            console.error("Failed to submit data");
+          }
+        })
+        .catch((error) => {
+          console.error("Error occurred while submitting data:", error);
+        });
+    } else {
+      console.error("Form validation failed:", validationResult.error);
+    }
   };
 
   return (
     <div className="">
-      <form
-        onSubmit={handleSubmit(submitData)}
-        className="flex w-full flex-col gap-8 border-2 border-zinc-950 rounded-lg px-8 py-4"
-      >
+      <div className="flex w-full flex-col gap-8 border-2 border-zinc-950 rounded-lg px-8 py-4">
         <div className="flex flex-col gap-4 justify-center items-start">
-          <Label htmlFor="text">Material Name</Label>
+          <Label htmlFor="r_name">Material Name</Label>
           <Input
+            name="r_name"
+            value={formData.r_name}
+            onChange={handleChange}
             className="w-[20vw]"
             type="text"
             placeholder="Cotton, Silk, Acrylic, etc."
           />
-          <Label htmlFor="text">Color</Label>
+          <Label htmlFor="r_color">Color</Label>
           <Input
+            name="r_color"
+            value={formData.r_color}
+            onChange={handleChange}
             className="w-[20vw]"
             type="text"
             placeholder="Blue, White, Black, etc."
           />
-          <Label htmlFor="text">Deniers</Label>
-          <Input className="w-[20vw]" type="text" placeholder="75, 150, etc." />
-          <Label htmlFor="text">Number of Filaments</Label>
+          <Label htmlFor="denier">Deniers</Label>
           <Input
+            name="denier"
+            value={formData.denier}
+            onChange={handleChange}
             className="w-[20vw]"
-            type="text"
+            type="number"
+            placeholder="75, 150, etc."
+          />
+          <Label htmlFor="filament">Number of Filaments</Label>
+          <Input
+            name="filament"
+            value={formData.filament}
+            onChange={handleChange}
+            className="w-[20vw]"
+            type="number"
             placeholder="12, 24, 36, etc."
           />
-          <Button className="bg-zinc-200 text-black hover:bg-zinc-100">
+
+          <Button
+            onClick={submitRawData}
+            className="bg-zinc-200 text-black hover:bg-zinc-100 active:scale-95"
+          >
             Submit
           </Button>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
