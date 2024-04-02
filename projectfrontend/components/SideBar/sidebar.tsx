a573b25b086620b3f377363aa09e31f3c1f4fefb
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-04-02 18:58:30 +0530

    added pagination to supplier information table

--- projectfrontend/components/SideBar/sidebar.tsx
+++ projectfrontend/components/SideBar/sidebar.tsx
@@ -17,11 +17,11 @@ const Sidebar = () => {
   ];
 
   return (
-    <div className="w-[20vw] bg-zinc-300  h-screen flex flex-col">
-      <div className="h-[8vh] w-[20vw] flex items-center justify-center gap-2 text-xl border-2 border-zinc-600 border-b-0">
+    <div className="bg-zinc-300  h-screen flex flex-col">
+      <div className="h-[8vh] flex items-center justify-center gap-2 text-xl border-2 border-zinc-600 border-b-0">
         T.W.M.S. <LuWarehouse />
       </div>
-      <div className="px-8 pt-4 h-full flex flex-col gap-4 border-2 border-zinc-600">
+      <div className="px-16 pt-4 h-full flex flex-col gap-4 border-2 border-zinc-600">
         {links.map((link, index) => (
           <Link key={index} href={link.href}>
             <h3 className="hover:text-zinc-600 transition-all duration-300 text-slate-950 w-24 gap-2 flex justify-start items-center">
