9680b5b93f25bfe820c8c69cc57619ce624d6e1e
Trijay1709 <trijaypatel1709@gmail.com>
2024-04-02 19:08:29 +0530

    added routes for raw material

--- projectbackend/index.js
+++ projectbackend/index.js
@@ -10,7 +10,7 @@ const conn = mysql.createConnection({
     host: "tdbms.mysql.database.azure.com",
     user: "ROOM1097",
     password: "@DBMSdiu",
-    database: "projet",
+    database: "projet", 
     port: 3306,
     ssl:{ca:fs.readFileSync("./DigiCertGlobalRootCA.crt.pem")}
 });
@@ -46,8 +46,6 @@ app.get("/page1",  (req, res) => {
         res.status(500).json({ error: "Internal Server Error" });
     }
 });
-
-
 app.get("/page2", (req, res) => {
     try {
         conn.query("SELECT val from test where id = 2", (error, results, fields) => {
@@ -106,14 +104,77 @@ app.get("/api/supplier/:id",(req,res)=>{
                 return res.status(500).json({ error: "Internal Server Error" });
             }
             console.log(results);
-            
-            res.json( results );
+            if(!results)
+            {
+                return response.status(404).send({message : 'Supplier not found'})
+            }
+            return res.json( results );
         });
     } catch (error) {
         console.error("Error querying database supplier specific: " + error.message);
         res.status(500).json({ error: "Internal Server Error" });
     }
 })
+app.get("/api/raw",(req,res)=>{
+    try {
+        conn.query("SELECT * FROM RAW_MATERIAL", (error, results, fields) => {
+            if (error) {
+                console.error("Error querying database: " + error.message);
+                return res.status(500).json({ error: "Internal Server Error" });
+            }
+            console.log(results);
+            res.json( results );
+        });
+    } catch (error) {
+        console.error("Error querying database: " + error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+})
+app.post("/api/raw", (req, res) => {
+    const R_name = req.body.R_name;
+    const R_color = req.body.R_color;
+    const denier = req.body.denier;
+    const no_of_filaments = req.body.no_of_filaments;
+    if (!R_name||!R_color||!denier||!no_of_filaments) {
+        console.error("Missing fields");
+        return res.status(400).send({message:'Send all required fields'});
+    }
+    try {
+        conn.query("INSERT INTO Raw_material (R_name, R_weight, R_color, denier, no_of_filaments) VALUES (?, ?, ?, ?, ?)", [R_name,0,R_color,denier,no_of_filaments], (error, results, fields) => {
+            if (error) {
+                console.error("Error querying database: " + error.message);
+                return res.status(500).json({ error: "Internal Server Error" });
+            }
+            // console.log(results);
+            res.json(results);
+        });
+    } catch (error) {
+        console.error("Error querying database: " + error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+app.get("/api/raw/:id",(req,res)=>{
+    const id = parseInt(req.params.id); 
+    try {
+        conn.query(`SELECT * FROM RAW_MATERIAL WHERE Rid = ${id}`, (error, results, fields) => {
+            if (error) {
+                console.error("Error querying database supplier specific: " + error.message);
+                return res.status(500).json({ error: "Internal Server Error" });
+            }
+            console.log(results);
+            if(!results)
+            {
+                return response.status(404).send({message : 'Raw_material not found'})
+            }
+            return res.json( results );
+        });
+    } catch (error) {
+        console.error("Error querying database supplier specific: " + error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
