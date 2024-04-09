c923af53f6879a12e8efee15f87a6b9d1f88e539
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-04-10 01:00:31 +0530

    completed form for supply logs along with data table.

--- projectfrontend/app/(logs)/(routes)/logs/page.tsx
+++ projectfrontend/app/(logs)/(routes)/logs/page.tsx
@@ -1,8 +1,10 @@
 import LogForm from "@/components/Logs/LogForm/LogForm";
+import LogTable from "@/components/Logs/LogTable/logTable";
 export default function Logs() {
   return (
     <div>
       <LogForm />
+      <LogTable />
     </div>
   );
 }
---
+++ projectfrontend/components/Logs/LogTable/logTable.tsx
@@ -0,0 +1,40 @@
+"use client";
+import React, { useState, useEffect } from "react";
+
+import { RawMaterialType, columns } from "./columns";
+import { DataTable } from "./data-table";
+
+const LogTable = () => {
+  const [data, setData] = useState<RawMaterialType[]>([]);
+
+  async function getData(): Promise<RawMaterialType[]> {
+    try {
+      const response = await fetch("http://localhost:3001/api/supply");
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const jsonData = await response.json();
+      return jsonData;
+    } catch (error) {
+      console.error("Error fetching supplier data:", error);
+      return [];
+    }
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const fetchedData = await getData();
+      setData(fetchedData);
+      console.log(data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
+};
+
+export default LogTable;
